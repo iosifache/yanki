8fdfd772f949e7bd2ef85b90155d75b9a4194140
iosifache <hi@iosifache.me>
2023-11-26 09:42:29 +0200

    Groups by type or language

--- yanki/main.py
+++ yanki/main.py
@@ -1,20 +1,24 @@
 """Dummy source file."""
 
+import os
 import random
+import sys
 import typing
 from dataclasses import dataclass
 
 import genanki
 import yaml
 
-DATA_FILE = "data.yaml"
-
 
 @dataclass
 class Entry:
     name: str
-    entry_type: str
+    language: str
+    entry_type: typing.Optional[str]
     meaning: str
+
+    def get_parent_deck(self) -> str:
+        return self.entry_type if self.entry_type else self.language
 
 
 @dataclass
@@ -23,24 +27,31 @@ class Deck:
     entries: typing.List[Entry]
 
 
-def read_data() -> dict:
-    with open(DATA_FILE) as file:
+def read_data(filename: str) -> dict:
+    with open(filename) as file:
         content = file.read()
         return yaml.safe_load(content)
 
 
 def get_notes(data: dict) -> typing.Generator[Entry, None, None]:
     for key, value in data.items():
-        yield Entry(key, value["type"], value["meaning"])
+        yield Entry(
+            key,
+            value["language"],
+            value.get("type", None),
+            value["meaning"],
+        )
 
 
 def generate_decks(entries: typing.List[Entry]) -> typing.List[Deck]:
     decks: dict[str, Deck] = {}
     for entry in entries:
-        parent_deck = decks.get(entry.entry_type, Deck(entry.entry_type, []))
+        parent_deck_name = entry.get_parent_deck()
+
+        parent_deck = decks.get(parent_deck_name, Deck(parent_deck_name, []))
         parent_deck.entries.append(entry)
 
-        decks[entry.entry_type] = parent_deck
+        decks[parent_deck_name] = parent_deck
 
     return list(decks.values())
 
@@ -72,32 +83,50 @@ def define_model() -> genanki.Model:
     )
 
 
-def dump_decks(model: genanki.Model, decks: typing.List[Deck]) -> None:
+def dump_decks(
+    model: genanki.Model,
+    decks: typing.List[Deck],
+    output_folder: str,
+) -> None:
     for deck in decks:
-        dump_deck(model, deck)
+        dump_deck(model, deck, output_folder)
 
 
-def dump_deck(model: genanki.Model, deck: Deck) -> None:
+def dump_deck(model: genanki.Model, deck: Deck, output_folder: str) -> None:
     out_deck = genanki.Deck(generate_id(), deck.name)
-
-    print(deck.name, deck.entries)
 
     for entry in deck.entries:
         note = genanki.Note(model=model, fields=[entry.name, entry.meaning])
         out_deck.add_note(note)
 
     # Save the deck to an Anki package (*.apkg) file
-    genanki.Package(out_deck).write_to_file(f"{deck.name}.apkg")
+    genanki.Package(out_deck).write_to_file(
+        f"{output_folder}/{deck.name}.apkg",
+    )
 
 
 def main() -> None:
     """Run the main functionality."""
+    if len(sys.argv) != 3:
+        print(f"Usage: {sys.argv[0]} YAML_FILENAME OUTPUT_FOLDER")
+        sys.exit(1)
+    filename = sys.argv[1]
+    output_folder = sys.argv[2]
+
+    if not os.path.isfile(filename):
+        print("The filename should point to an existent YAML file.")
+        sys.exit(1)
+
+    if not os.path.isdir(output_folder):
+        print("The output folder should exist.")
+        sys.exit(1)
+
     model = define_model()
 
-    data = read_data()
+    data = read_data(filename)
     notes = list(get_notes(data))
     decks = generate_decks(notes)
-    dump_decks(model, decks)
+    dump_decks(model, decks, output_folder)
 
 
 if __name__ == "__main__":
